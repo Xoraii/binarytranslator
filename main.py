20d290d0e47dfde251c56fb65ddaf42b504d7819
Xoraii <119786050+Xoraii@users.noreply.github.com>
2022-12-04 15:46:54 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+def binario(decimal):
+    binario = ""
+    while decimal // 2 != 0:
+        binario = str(decimal % 2) + binario
+        decimal = decimal // 2
+    return str(decimal) + binario
+
+numero = int(input("What number do you want to convert to binary?: "))
+print(binario(numero))
